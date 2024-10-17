e9f7ac18c1aaf79a423fdec4c9e4015143c48a3b
Torsten Wylegala <mail@twyleg.de>
2024-10-17 16:14:30 +0200

    Trying to resolve test issues on windows

--- tests/test_image_layer_extraction.py
+++ tests/test_image_layer_extraction.py
@@ -155,14 +155,17 @@ class Image0TestCase(ImageTestCase):
 
         self.assert_mtime_newer(extracted_image_file_paths_by_layer_paths["/"], self.test_image_path)
 
-        time.sleep(0.1)
+        time.sleep(1.0)
         self.test_image_path.touch()
+        time.sleep(1.0)
 
         self.assert_mtime_newer(self.test_image_path, extracted_image_file_paths_by_layer_paths["/"])
 
-        self.test_image = self.prepare_test_image()
+        # self.test_image = self.prepare_test_image()
 
         extracted_image_file_paths_by_layer_paths = self.test_image.extract_all_layers_to_file_lazy(layer_output_dir_path, "base_name", self.test_image_path)
+
+        del self.test_image
 
         self.assert_mtime_newer(extracted_image_file_paths_by_layer_paths["/"], self.test_image_path)
 
